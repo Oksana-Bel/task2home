b35e668cd746cdf0074d1ef4a3d8246c0b7d62e7
Oksana-Bel <belousova.oksana.work@gmail.com>
2021-03-22 06:53:19 -0400

    add test2home

--- src/test/java/Test2home.java
+++ src/test/java/Test2home.java
@@ -10,6 +10,8 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Test2home {
+
+
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
